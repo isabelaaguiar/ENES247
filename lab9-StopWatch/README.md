e787a175843d0b0f235eb1094bb36b3c678d1456
isabelaaguiar <isabela.porfiriodeaguiar@howardcc.edu>
2019-05-10 15:05:05 -0400

    LAB_9_1_2 done

--- lab9-StopWatch/README.md
+++ lab9-StopWatch/README.md
@@ -36,6 +36,8 @@ p and g are look-ahead adders that improve the speed by reducing the amount of t
 
 Testbench creates a cout_expected and s_expected. Can see the difference between the expected values and actul values in the testbench, which are caused by the delays.
 
+On Vivado, I could click on the edges and observe that the difference in time between the 
+
 
 
 
@@ -43,3 +45,34 @@ Testbench creates a cout_expected and s_expected. Can see the difference between
 ## Lab 9_1_2
 
 #### Verilog Code
+
+![1557511453301](1557511453301.png)
+
+Can see modification in the code, from the previous one.
+
+In part 1 (9_1_1). the parameters were defined in the main verilog code. Can see the  #(3, 3, 4) in the code. And passed to the modules. 
+
+For this part (9_1_2), the parameters were not defined in the main verilog code. Instead, the "defparam"
+
+ tool was used in the testbench.
+
+
+
+#### RTL
+
+![1557505861068](C:\Users\SET253-19U.HCCMAIN\AppData\Roaming\Typora\typora-user-images\1557505861068.png)
+
+RTL did not change
+
+
+
+#### Testbench
+
+
+
+![1557511752014](1557511752014.png)
+
+Defparam statements were added to the testbench , changing the values of the delays.
+
+
+
--- lab9-StopWatch/lab9_1_1/iporfirio_9_1_1/iporfirio_9_1_1.sim/sim_1/behav/xsim/xsim.dir/claadder_gate_tb_behav/webtalk/xsim_webtalk.tcl
+++ lab9-StopWatch/lab9_1_1/iporfirio_9_1_1/iporfirio_9_1_1.sim/sim_1/behav/xsim/xsim.dir/claadder_gate_tb_behav/webtalk/xsim_webtalk.tcl
@@ -1,6 +1,6 @@
 webtalk_init -webtalk_dir C:/Users/SET253-19U.HCCMAIN/Documents/GitHub/ENES247/lab9-StopWatch/lab9_1_1/iporfirio_9_1_1/iporfirio_9_1_1.sim/sim_1/behav/xsim/xsim.dir/claadder_gate_tb_behav/webtalk/
 webtalk_register_client -client project
-webtalk_add_data -client project -key date_generated -value "Fri May 10 13:35:26 2019" -context "software_version_and_target_device"
+webtalk_add_data -client project -key date_generated -value "Fri May 10 15:02:11 2019" -context "software_version_and_target_device"
 webtalk_add_data -client project -key product_version -value "XSIM v2018.3 (64-bit)" -context "software_version_and_target_device"
 webtalk_add_data -client project -key build_version -value "2405991" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_platform -value "WIN64" -context "software_version_and_target_device"
@@ -14,7 +14,7 @@ webtalk_add_data -client project -key target_package -value "not_applicable" -co
 webtalk_add_data -client project -key target_speed -value "not_applicable" -context "software_version_and_target_device"
 webtalk_add_data -client project -key random_id -value "384761e1-15ed-4fc6-9d0a-4b1c41256b19" -context "software_version_and_target_device"
 webtalk_add_data -client project -key project_id -value "68662a0fc8cb4f17849965af29775bc2" -context "software_version_and_target_device"
-webtalk_add_data -client project -key project_iteration -value "3" -context "software_version_and_target_device"
+webtalk_add_data -client project -key project_iteration -value "4" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_name -value "Microsoft Windows 8 or later , 64-bit" -context "user_environment"
 webtalk_add_data -client project -key os_release -value "major release  (build 9200)" -context "user_environment"
 webtalk_add_data -client project -key cpu_name -value "Intel(R) Core(TM) i5-6500 CPU @ 3.20GHz" -context "user_environment"
@@ -26,7 +26,7 @@ webtalk_add_data -client xsim -key Command -value "xsim" -context "xsim\\command
 webtalk_add_data -client xsim -key trace_waveform -value "true" -context "xsim\\usage"
 webtalk_add_data -client xsim -key runtime -value "1 us" -context "xsim\\usage"
 webtalk_add_data -client xsim -key iteration -value "0" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Time -value "0.03_sec" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Memory -value "6364_KB" -context "xsim\\usage"
-webtalk_transmit -clientid 3647689638 -regid "" -xml C:/Users/SET253-19U.HCCMAIN/Documents/GitHub/ENES247/lab9-StopWatch/lab9_1_1/iporfirio_9_1_1/iporfirio_9_1_1.sim/sim_1/behav/xsim/xsim.dir/claadder_gate_tb_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/Users/SET253-19U.HCCMAIN/Documents/GitHub/ENES247/lab9-StopWatch/lab9_1_1/iporfirio_9_1_1/iporfirio_9_1_1.sim/sim_1/behav/xsim/xsim.dir/claadder_gate_tb_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/Users/SET253-19U.HCCMAIN/Documents/GitHub/ENES247/lab9-StopWatch/lab9_1_1/iporfirio_9_1_1/iporfirio_9_1_1.sim/sim_1/behav/xsim/xsim.dir/claadder_gate_tb_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
+webtalk_add_data -client xsim -key Simulation_Time -value "0.01_sec" -context "xsim\\usage"
+webtalk_add_data -client xsim -key Simulation_Memory -value "6352_KB" -context "xsim\\usage"
+webtalk_transmit -clientid 3346162248 -regid "" -xml C:/Users/SET253-19U.HCCMAIN/Documents/GitHub/ENES247/lab9-StopWatch/lab9_1_1/iporfirio_9_1_1/iporfirio_9_1_1.sim/sim_1/behav/xsim/xsim.dir/claadder_gate_tb_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/Users/SET253-19U.HCCMAIN/Documents/GitHub/ENES247/lab9-StopWatch/lab9_1_1/iporfirio_9_1_1/iporfirio_9_1_1.sim/sim_1/behav/xsim/xsim.dir/claadder_gate_tb_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/Users/SET253-19U.HCCMAIN/Documents/GitHub/ENES247/lab9-StopWatch/lab9_1_1/iporfirio_9_1_1/iporfirio_9_1_1.sim/sim_1/behav/xsim/xsim.dir/claadder_gate_tb_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
 webtalk_terminate
